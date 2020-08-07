2fe809c13feb06d705a34d0b3718607913bb0d8d
Deonte Archie <deontearchie@icloud.com>
2020-08-06 23:25:10 -0400

    Gotten my front page to work and i put my questions in the javascript

---
+++ Question.js
@@ -0,0 +1,34 @@
+var avQuestions = [{
+        question: "How long was Aang gone from the world?",
+        Answer: {
+            a: "50yrs",
+            b: "1000yrs",
+            c: "100yrs"
+
+        },
+        correctAnswer: "c"
+
+    },
+    {
+        question: "what was the Fire Lord name that Aang had to defeat?",
+        Answer: {
+            a: "Roku",
+            b: "Ozai",
+            c: "Zoku"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What was the name of Aang of Bison?",
+        Answer: {
+            a: "MoMo",
+            b: "Oppa",
+            c: "Katara"
+        },
+        correctAnswer: "b"
+
+
+    }
+
+
+];
